3eaba6fff1be754e5b88bd923eab27deb4b2d296
jroquebento <jroquebento@gmail.com>
2023-08-16 09:17:37 -0300

    exemplo com operador ternario

---
+++ tipos-variaveis/src/Ternario.java
@@ -0,0 +1,13 @@
+public class Ternario {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        //String resultado = a == b ? "verdadeiro" : "falso";
+        int resultado = a == b ? 1 : 0;
+         
+        System.out.println(resultado);  
+    }
+}
