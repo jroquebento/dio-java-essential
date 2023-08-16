fc3653f5c111b6cebc0ef9677615211e091ca2a0
jroquebento <jroquebento@gmail.com>
2023-08-16 09:16:22 -0300

    exemplo operadores negacao e decremento

---
+++ tipos-variaveis/src/OperadoresUnarios2.java
@@ -0,0 +1,15 @@
+public class OperadoresUnarios2 {
+    public static void main(String[] args) {
+        boolean variavel = true;
+        variavel = !variavel;
+        System.out.println(variavel);
+    
+        //x repeticao
+
+        //numero++ == numero = numero + 1
+
+        int numero = 5;
+        System.out.println(numero--);
+        System.out.println(numero);
+    }
+}
