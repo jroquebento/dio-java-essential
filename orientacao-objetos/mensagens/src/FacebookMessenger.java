60b9ffa21fdcceb760f074232dc568a83acce7b0
jroquebento <jroquebento@gmail.com>
2023-09-11 20:06:35 -0300

    exemplo de abstracao

--- orientacao-objetos/mensagens/src/FacebookMessenger.java
+++ orientacao-objetos/mensagens/src/FacebookMessenger.java
@@ -1,3 +1,12 @@
 public class FacebookMessenger extends ServicoMensagemInstantanea{
+
+    @Override
+    public void enviarMensagem() {
+       System.out.println("Enviando mensagem pelo Facebook Messenger");
+    }
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo Facebook Messenger");
+    }
 	
 }
--- orientacao-objetos/mensagens/src/Telegram.java
+++ orientacao-objetos/mensagens/src/Telegram.java
@@ -1,3 +1,12 @@
 public class Telegram extends ServicoMensagemInstantanea {
+
+    @Override
+    public void enviarMensagem() {
+       System.out.println("Enviando mensagem pelo Telegram");
+    }
+    @Override
+    public void receberMensagem() {
+       System.out.println("Recebendo mensagem pelo Telegram");
+    }
 	
 }
