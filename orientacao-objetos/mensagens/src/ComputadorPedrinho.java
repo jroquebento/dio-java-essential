3ad0fe4ce50db03f175f965a5f56ab1dabccce2b
jroquebento <jroquebento@gmail.com>
2023-09-11 19:57:20 -0300

    exemplo de herenca

--- orientacao-objetos/mensagens/src/ComputadorPedrinho.java
+++ orientacao-objetos/mensagens/src/ComputadorPedrinho.java
@@ -4,9 +4,19 @@
 public class ComputadorPedrinho {
 	public static void main(String[] args) {
 		//abrindo MSN Messenger
+		System.out.println("MSN");
 		MSNMessenger msn = new MSNMessenger();
-		
 		msn.enviarMensagem();
 		msn.receberMensagem();
+	
+		System.out.println("FACEBOOK");
+        FacebookMessenger fcb = new FacebookMessenger();
+		fcb.enviarMensagem();
+		fcb.receberMensagem();
+
+		System.out.println("TELEGRAM");
+        Telegram tlg = new Telegram();
+		tlg.enviarMensagem();
+		tlg.receberMensagem();
 	}
 }
